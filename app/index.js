14b55e910131e04c4451e733e1cbadaa3a9fec97
Victoria Kaihe <tori.ellen.g.k@gmail.com>
2019-01-04 15:37:33 +1300

    remove searchpage from index.js

--- app/index.js
+++ app/index.js
@@ -1,32 +1,8 @@
 'use strict' 
 
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
-import { StackNavigator } from 'react-navigation';
-
-
-class SearchPage extends React.Component {
-  static navigationOptions = {
-    title: 'Property Finder'
-  }
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Search for houses to buy!</Text>
-      </View>
-    )
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    fontSize: 18,
-    color: '#656565',
-    marginTop: 50,
-  },
-})
+import { StackNavigator } from 'react-navigation'
+import SearchPage from '../SearchPage'
 
 const App = StackNavigator({
   Home: { screen: SearchPage },
