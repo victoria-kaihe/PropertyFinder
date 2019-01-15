b592c1885e0a28f6ea86b070c9d7fd350b0b207e
Victoria Kaihe <tori.ellen.g.k@gmail.com>
2019-01-16 12:16:26 +1300

    add render to search results

--- SearchResults.js
+++ SearchResults.js
@@ -9,3 +9,28 @@ import {
   Flatlist,
   Text,
 } from 'react-native'
+
+export default class SearchResults extends Component<{}> {
+  _keyExtractor = (item, index) => index
+
+  _renderItem = ({item}) => {
+    return (
+      <TouchableHighlight
+      underlayColor='#dddddd'>
+      <View>
+        <Text>{item.title}</Text>
+      </View>
+      </TouchableHighlight>
+    )
+  }
+
+  render() {
+    return (
+      <Flatlist
+      data={this.propslistings}
+      keyExtractor={this._keyExtractor}
+      renderItem={this._renderItem}
+      />
+    )
+  }
+}
